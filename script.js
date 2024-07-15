f95acddb1a0b5266358b5e744fbd5c75f13eb16b
Megha Manoj <manojmegha2002@gmail.com>
2024-07-15 14:53:16 +0400

    update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
  
+
 const menuicon= document.querySelector('#menu-icon');
 const navbar= document.querySelector('.navbar');
-
-
-
 menuicon.onclick = () => {
     menuicon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
